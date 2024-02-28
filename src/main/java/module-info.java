20da9881d00e8df13c069af1f07a5718d72e817e
Zakaria Farih <zakariafarih142@gmail.com>
2024-02-28 13:41:33 +0100

    Initial commit - Creating JFX Project - Adding Maven Dependencies

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cesur.zakaria.pokemonprojectzakariafarih {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cesur.zakaria.pokemonprojectzakariafarih to javafx.fxml;
+    exports cesur.zakaria.pokemonprojectzakariafarih;
+}
